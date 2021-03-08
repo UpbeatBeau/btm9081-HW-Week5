b045189320b90b9d8e6780509a5e33ca9d39775d
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2021-03-08 18:20:16 -0500

    Added the unity project

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) //if something enters the trigger
+    {
+        //Increase the Score Property in the ASCIILevelLoader
+        //that we reference through the GameManager Singleton
+        GameManager.instance.GetComponent<ASCIILevelLoader>().CurrentLevel++;
+        
+    }
+}
